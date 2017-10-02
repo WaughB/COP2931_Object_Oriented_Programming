22cc92a55e0d8d3711671fc58d35b10ed053df3f
Brett Waugh <brett.waugh2@gmail.com>
2017-10-02 17:47:58 -0400

    Added week1_p1 and created folders for some of the weeks

---
+++ week1_p1.cpp
@@ -0,0 +1,25 @@
+/**********************************
+/   Brett Waugh
+/   25 August 2017
+/   Program gets an integer and
+/	returns the entered integer.
+***********************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declarations
+    int number; // Integer was specified in instructions. 
+    
+    // Input
+    cout<<"Please enter an integer...\n"; // Asks User for integer. 
+    
+    cin>>number; // Gets input and assigns to variable. 
+    
+    // Output
+    cout<<"Your integer was " << number; // Displays the same number. 
+
+    return 0;
+}
